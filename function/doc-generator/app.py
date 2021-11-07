f62d39424e8acf3f0445cc9aed0571f061ee4ae8
Tomaz Foa Binsztajn <tomaz.fb@gmail.com>
2021-11-07 18:40:18 -0300

    v0 da função

--- function/doc-generator/app.py
+++ function/doc-generator/app.py
@@ -1,7 +1,22 @@
 import json
+import os
+import boto3
+import logging
 
 # import requests
 
+
+def __init():
+    event = {
+        "template": "meutemplate.html",
+        "variables": {
+            "nome": "Fulanto de Tal",
+            "cpf": "123.456.789-01"
+        }
+    }
+    os.environ["TemplatesS3Bucket"] = "fuzzy-doodle-templates"
+
+    lambda_handler(event, None)
 
 def lambda_handler(event, context):
     """Sample pure Lambda function
@@ -24,7 +39,29 @@ def lambda_handler(event, context):
 
         Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
     """
+    logging.basicConfig(level=logging.INFO)
 
+    bucket = os.environ['TemplatesS3Bucket'] # this comes from template.yml configuration and its set on ci/cd time
+    logging.info("bucket=%s",bucket)
+
+    template = event['template']
+    logging.info("template=%s",template)
+
+    variables = event['variables'] #dict
+    logging.info("variables=%s", variables)
+
+    s3 = boto3.client('s3')
+    s3_object = s3.get_object(Bucket=bucket, Key=template)
+    body = s3_object['Body']
+    content = body.read()
+    
+
+    ## read template
+    result = str(content)
+    for key, value in variables.items():
+        result = result.replace("${"+key+"}", value)
+
+    #log.info(result)
     # try:
     #     ip = requests.get("http://checkip.amazonaws.com/")
     # except requests.RequestException as e:
@@ -35,8 +72,12 @@ def lambda_handler(event, context):
 
     return {
         "statusCode": 200,
-        "body": json.dumps({
-            "message": "hello world",
-            # "location": ip.text.replace("\n", "")
-        }),
+        "body": result
+        #json.dumps({
+        #    "message": "hello world",
+        #    # "location": ip.text.replace("\n", "")
+        #}),
     }
+
+
+#__init()
